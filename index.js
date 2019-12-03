e0c0d993ff61bcaa98b8cf3b429d032ee80bad97
Shadow Smith <shadow@businesscreditreports.com>
2019-12-03 14:23:55 -0600

    Add temp test molecule function

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 const atom = require("./js/atoms");
 const molecule = require("./js/molecules");
 const organism = require("./js/organisms");
+
+// Left here for development
+molecule.revertSecureData("XQAAgAAHAAAAAAAAAAARXQAAgAA+AAAAAAAAAAARCQKtAAJRteTYRNfLztqH6ZIt3n8biMYa+TAXmPWfGuOpp67Jq1VDhOa62MSo/GlIU5pJBvTWESIWDSo7DGNpqnh5PP//h1YAABUJBlPIHPOfeP///z5YAAA=", "Hello");
