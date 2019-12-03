0b758dce5a245d5d3ff2075186e6de53de29fac4
Shadow Smith <shadow@businesscreditreports.com>
2019-12-03 14:23:19 -0600

    Add write and readSecureCorvidorFile organisms

--- js/organisms.js
+++ js/organisms.js
@@ -5,8 +5,16 @@ function writeCorvidorFile(data) {
     atom.createFile(atom.createFileName(), molecule.convertData(data));
 }
 
+function writeSecureCorvidorFile(data, secret) {
+    atom.createFile(atom.createFileName(), molecule.convertSecureData(data, secret));
+}
+
 function readCorvidorFile(name) {
     return molecule.revertData(atom.readFile(name));
+}
+
+function readSecureCorvidorFile(name, secret) {
+    return molecule.revertSecureData(atom.readFile(name));
 }
 
 function updateCorvidorFile(name, data) {
@@ -15,6 +23,8 @@ function updateCorvidorFile(name, data) {
 
 module.exports = {
     writeCorvidorFile,
+    writeSecureCorvidorFile,
     readCorvidorFile,
+    readSecureCorvidorFile,
     updateCorvidorFile
 };
