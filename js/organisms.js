4b28aaf155575f7c03295e6350bc149d39b65ae8
Shadow Smith <shadow@businesscreditreports.com>
2019-12-03 11:32:10 -0600

    Add updateCorvidorFile organism function

--- js/organisms.js
+++ js/organisms.js
@@ -2,14 +2,19 @@ const atom = require("./atoms");
 const molecule = require("./molecules");
 
 function writeCorvidorFile(data) {
-    atom.createFile(atom.createFileName(), molecule.convertData(data))
+    atom.createFile(atom.createFileName(), molecule.convertData(data));
 }
 
 function readCorvidorFile(name) {
     return molecule.revertData(atom.readFile(name));
 }
 
+function updateCorvidorFile(name, data) {
+    atom.createFile(name, molecule.convertData(data));
+}
+
 module.exports = {
     writeCorvidorFile,
-    readCorvidorFile
+    readCorvidorFile,
+    updateCorvidorFile
 };
