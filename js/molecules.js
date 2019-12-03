e53ab74c797b5a6b6a999b1fa850531951110280
Shadow Smith <shadow@businesscreditreports.com>
2019-12-03 14:22:23 -0600

    Add more molecule functions

--- js/molecules.js
+++ js/molecules.js
@@ -7,6 +7,16 @@ function convertData(data) {
     return encodedData;
 }
 
+function convertSecureData(data, secret) {
+    const compressedData = atom.compressData(data);
+    const encryptedData = atom.encryptData(secret);
+    const compressedEncryptedData = atom.compressData(encryptedData);
+    const blendedCompressedData = blendCompressedData(compressedData, compressedEncryptedData);
+    const bufferedData = atom.bufferData(blendedCompressedData);
+    const encodedData = atom.encodeData(bufferedData);
+    return encodedData; 
+}
+
 function revertData(data) {
     const decodedData = atom.decodeData(data);
     const debufferedData = atom.debufferData(decodedData);
@@ -14,7 +24,34 @@ function revertData(data) {
     return decompressedData;
 }
 
+function revertSecureData(data, secret) {
+    const decodeData = atom.decodeData(data);
+    const debufferedData = atom.debufferData(decodeData);
+    return splitBlendedCompressedData(debufferedData, secret);
+}
+
+function blendCompressedData(data1, data2) {
+    const arrayCenter = atom.findArrayCenter(data1);
+    const newArray = atom.combineArray(data1, arrayCenter, data2);
+    return newArray;
+}
+
+function splitBlendedCompressedData(data, secret) {
+    const encryptedData = atom.encryptData(secret);
+    const compressedEncryptedData = atom.compressData(encryptedData);
+    findArrayInArray(data, compressedEncryptedData);
+}
+
+function findArrayInArray(array1, array2) {
+    console.log(array1);
+    console.log(array2);
+}
+
 module.exports = {
     convertData,
-    revertData
+    convertSecureData,
+    revertData,
+    revertSecureData,
+    blendCompressedData,
+    splitBlendedCompressedData
 };
