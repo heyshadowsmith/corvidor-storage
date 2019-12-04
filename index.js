9328e94664940322cb100e5f4e3f0d7e3d935089
Shadow Smith <shadow@businesscreditreports.com>
2019-12-04 16:15:43 -0600

    Update index for development

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ const atom = require("./js/atoms");
 const molecule = require("./js/molecules");
 const organism = require("./js/organisms");
 
-// console.log(organism.readSecureCorvidorFile("5Lz3yCmL", "Smith"));
-console.log(molecule.revertSecureData(atom.readFile("5Lz3yCmL"), "Smith"));
+// organism.writeSecureCorvidorFile({
+//   "name": "shadow",
+//   "age": "27"
+// }, "Smith");
+console.log(organism.readSecureCorvidorFile("u_Q0e52H", "Smith"))
