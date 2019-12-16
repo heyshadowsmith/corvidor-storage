3a99bf680dd523f4535369547bd3b5832011afcf
Shadow Smith <shadow@businesscreditreports.com>
2019-12-16 15:21:29 -0600

    Update index for development

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
-const atom = require("./js/atoms");
-const molecule = require("./js/molecules");
 const organism = require("./js/organisms");
 
 // CREATE
-// console.log(JSON.stringify(organism.writeSecureCorvidorFile(data, secret), undefined, 4));
+// console.log(JSON.stringify(organism.writeCorvidorFile(data, secret), undefined, 4));
 
 // READ
-// console.log(JSON.stringify(organism.readSecureCorvidorFile(name, secret), undefined, 4));
+// console.log(JSON.stringify(organism.readCorvidorFile(name, secret), undefined, 4));
 
 // UPDATE
-// console.log(JSON.stringify(organism.updateSecureCorvidorFile(name, data, secret), undefined, 4));
+// console.log(JSON.stringify(organism.updateCorvidorFile(name, data, secret), undefined, 4));
 
 // DESTROY
-// console.log(JSON.stringify(organism.deleteSecureCorvidorFile(name, secret), undefined, 4));
+// console.log(JSON.stringify(organism.deleteCorvidorFile(name, secret), undefined, 4));
