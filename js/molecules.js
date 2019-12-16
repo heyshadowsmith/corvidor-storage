eadfde1f4f2a78228600bca5dd24fab2022b1b36
Shadow Smith <shadow@businesscreditreports.com>
2019-12-16 15:13:05 -0600

    Update & removed molecules

--- js/molecules.js
+++ js/molecules.js
@@ -1,12 +1,5 @@
 const atom = require("./atoms");
 
-function convertData(data) {
-    const compressedData = atom.compressData(data);
-    const bufferedData = atom.bufferData(compressedData);
-    const encodedData = atom.encodeData(bufferedData);
-    return encodedData;
-}
-
 function convertSecureData(data, secret) {
     const compressedData = atom.compressData(data);
     const compressedSecret = atom.compressData(secret);
@@ -16,25 +9,29 @@ function convertSecureData(data, secret) {
     return encodedData;
 }
 
-function revertData(data) {
-    const decodedData = atom.decodeData(data);
-    const debufferedData = atom.debufferData(decodedData);
-    const decompressedData = atom.decompressData(debufferedData);
-    return decompressedData;
-}
-
 function revertSecureData(data, secret) {
     const decodedData = atom.decodeData(data);
     const debufferedData = atom.debufferData(decodedData);
     const compressedSecret = atom.compressData(secret)
-    const originalArray = atom.getOriginalArray(debufferedData, compressedSecret)
-    const decompressedData = atom.decompressData(originalArray);
+    const originalArrays = atom.getOriginalArrays(debufferedData, compressedSecret)
+    const decompressedData = atom.decompressData(originalArrays[0]);
     return decompressedData;
 }
 
+function secretsMatch(data, secret) {
+    const decodedData = atom.decodeData(data);
+    const debufferedData = atom.debufferData(decodedData);
+    const compressedSecret = atom.compressData(secret);
+    const originalArrays = atom.getOriginalArrays(debufferedData, compressedSecret);
+    const decompressedSecret = atom.decompressData(originalArrays[1]);
+
+    if (secret !== decompressedSecret) return false
+
+    return true;
+}
+
 module.exports = {
-    convertData,
     convertSecureData,
-    revertData,
-    revertSecureData
+    revertSecureData,
+    secretsMatch
 };
