239db53dce4750db7ccd1f4687d707dad457f1cb
Shadow Smith <shadow@businesscreditreports.com>
2019-12-17 14:43:24 -0600

    Update atoms test location

--- tests/atoms.test.js
+++ tests/atoms.test.js
@@ -1,4 +1,4 @@
-const atom = require("./atoms");
+const atom = require("../js/atoms");
 
 const compressedData = atom.compressData("Hello");
 const combinedData = atom.combineData(compressedData, compressedData);
@@ -10,6 +10,11 @@ const splitData = atom.splitData(debufferedData, compressedData);
 const decompressedData = atom.decompressData(splitData[0]);
 
 // UNIT TESTS
+test("Create file", async () => {
+  const response = await atom.createFile("TestFile", "Hello");
+  expect(response).toEqual("TestFile has been added to Corvidor");
+});
+
 test("Compress data", () => {
   expect(compressedData).toEqual([0, 0, 0, -128, 0, 7, 0, 0, 0, 0, 0, 0, 0, 0, 17, 18, 8, -90, -52, 85, -52, 89, -33, -65, -1, -1, -16, -124, 0, 0]);
 });
@@ -51,6 +56,26 @@ test("Decompress data", () => {
   expect(decompressedData).toEqual("Hello");
 });
 
-test("File name generated", () => {
+test("Generate file name", () => {
   expect(typeof atom.createFileName()).toBe("string");
-})
+});
+
+test("Read file", async () => {
+  const fileData = await atom.readFile("TestFile");
+  expect(fileData).toEqual("Hello");
+});
+
+test("Can't read non-existent file", async () => {
+  const response = await atom.readFile("NoFile");
+  expect(response).toEqual("NoFile is not an id in Corvidor");
+});
+
+test("Delete file", async () => {
+  const response = await atom.deleteFile("TestFile");
+  expect(response).toEqual("TestFile has been deleted from Corvidor");
+});
+
+test("Can't delete non-existent file", async () => {
+  const response = await atom.deleteFile("NoFile");
+  expect(response).toEqual("NoFile is not an id in Corvidor");
+});
